eea95bd7826ff52170b81330f01d08ce5c66fa64
sudhanv <mystogun125@gmail.com>
2022-10-21 04:24:43 +0800

    Init Repo

---
+++ scripts/datagen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from PIL import Image
+
+
+class DataBuilder:
+    ''' Class for dataset related methods '''
+    def __init__(self):
+        self.fixed_size = 200
+
+    def superimpose(self, img1, img2):
+        ''' Method for superimposing image on top of each other
+        '''
+
+    def resize_image(self, image):
+        """ Method for resizing image"""
